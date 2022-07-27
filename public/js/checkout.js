dacff9887868f36959e50399f51166bb2eb7d959
avnotaklu <sukhmander111101@gmail.com>
2022-07-27 22:09:43 +0530

    checkout toast fix yuvraj thoda dhyan se code pdha kro ;)

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,19 +1,16 @@
 import {
     doc,
-    setDoc,
+    updateDoc,
     getFirestore,
 } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore-lite.js";
 import { getUserEmail } from "./services/firebase.js";
 
-const clearCart = async() => {
+async function clearCart() {
     const db = getFirestore();
     var userCredentials = JSON.parse(localStorage.getItem("user"));
-        await setDoc(
-            doc(db, "user", getUserEmail()), {
-                cart: null
-            },
-        ).then(Toast.show("Checked Out", "Sucessfully", "success"));
-        location.replace('index.html#');
-
-};
-document.getElementById("checkOut").addEventListener("click", clearCart());
+    await updateDoc(doc(db, "user", getUserEmail()), {
+        cart: null,
+    }).then(Toast.show("Checked Out", "Sucessfully", "success"));
+    location.replace("index.html#");
+}
+document.getElementById("checkOut").addEventListener("click", clearCart);
